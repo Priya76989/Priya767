a4d4f3d5d7dfad54a8bfc6ebfb6300a4a5a2652b
Priya76989 <Priya.Shrestha@triosstudent.com>
2024-10-16 12:02:48 -0400

    Add files via upload

---
+++ priya11/src/main/java/org/example/priya11/Main.java
@@ -0,0 +1,23 @@
+package org.example.priya11;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginView.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Login Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
